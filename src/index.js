68c91c4be8c5ac66805cd858d789de96d7a101cd
InesParis <inesparisarranz@gmail.com>
2024-07-28 17:27:13 +0200

    css
    Adding the css design to the file.
    Checking the html

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
-function displayCurrentTime(event) {
-  if (event.target.value.length > 0) {
-    let currentTime = moment()
-      .tz(event.target.value)
-      .format("MMMM D, YYYY HH:MM A");
-    alert(`It is currently ${currentTime} in ${event.target.value}`);
-  }
-}
-let currentTime = document.querySelector("#city");
-currentTime.addEventListener("change", displayCurrentTime);
+
