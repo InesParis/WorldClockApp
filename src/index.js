136e39c39b823f7cdad912e84311f569aa32ebf1
InesParis <inesparisarranz@gmail.com>
2024-07-28 15:22:36 +0200

    fixing code

--- src/index.js
+++ src/index.js
@@ -1,35 +1,10 @@
-let currentTime;
-document
-  .querySelector("#countries")
-  .addEventListener("change", displayCurrentTime);
-document
-  .querySelector("#countries")
-  .addEventListener("change", displayCurrentTimeZone);
-
 function displayCurrentTime(event) {
-  let selectedOption = event.target.value;
-  let currentTime;
-  if (selectedOption === "tokyo") {
-    currentTime = moment().tz("Asia/Tokyo").format("MMMM D, YYYY HH:MM A");
-  } else if (selectedOption === "paris") {
-    currentTime = moment().tz("Europe/ Paris").format("MMMM D, YYYY HH:MM A");
-  } else if (selectedOption === "sydney") {
-    currentTime = moment()
-      .tz("Australia/ Sydney")
+  if (event.target.value.length > 0) {
+    let currentTime = moment()
+      .tz(event.target.value)
       .format("MMMM D, YYYY HH:MM A");
+    alert(`It is currently ${currentTime} in ${event.target.value}`);
   }
 }
-
-function displayCurrentTimeZone(event) {
-  let selectedOption = event.target.value;
-  let currentTimeZone;
-
-  if (selectedOption === "tokyo") {
-    currentTimeZone = moment().tz("Asia/Tokyo");
-  } else if (selectedOption === "paris") {
-    currentTimeZone = moment().tz("Eurpe/Paris");
-  } else if (selectedOption === "sydney") {
-    currentTimeZone = moment().tz("Australia/Sydney");
-  }
-  alert(`It is currently ${currentTime} in ${currentTimeZone}`);
-}
+let currentTime = document.querySelector("#city");
+currentTime.addEventListener("change", displayCurrentTime);
